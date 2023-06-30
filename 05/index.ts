6b466322b91826a6d58fa4c91e424b42dbdb7438
btbrad <24836323+btbrad@users.noreply.github.com>
2023-06-30 23:39:28 +0800

    05

--- 05/index.ts
+++ 05/index.ts
@@ -2,52 +2,45 @@ function foo(name: string): number {
   return name.length
 }
 
-const foo2 = function(name: string): number {
+const foo1 = function (name: string): number {
   return name.length
 }
 
-const foo3: (name: string) => number = (name) => {
+const foo2: (name: string) => number = function (name) {
   return name.length
 }
 
-const foo4 = (name: string): number => {
-  return  name.length
+const foo3 = (name: string): number => {
+  return name.length
 }
 
-type FuncFoo = (name: string) => number
-const foo5: FuncFoo = (name) => {
+const foo4: (name: string) => number = (name) => {
+  return name.length
+}
+
+type FuncFood = (name: string) => number
+const foo5: FuncFood = (name) => {
   return name.length
 }
 
 interface FuncFooStruct {
   (name: string): number
 }
-const foo6: FuncFooStruct = (name: string) => {
-  return name.length
-}
 
-function foo7(): void {}
-function foo8(): undefined {
+function foo6(): void {}
+function bar(): undefined {
   return
 }
 
-function foo9(arg1: string, ...rest: any[]) {}
-foo9('brad', 1,2,3,4)
-function foo10(arg1: string, ...rest: [number, boolean]) {}
-foo10('brad', 100, true)
-
-
-function func(foo: number, bar: true): string;
-function func(foo: number, bar?: false): number;
-function func(foo: number, bar?: boolean): string | number {
-  if (bar) {
-    return foo.toString()
-  } else {
-    return foo*599
-  }
+function foo7(name: string, age?: number): number {
+  const inputAge = age || 18
+  return name.length + inputAge
 }
-console.log(func(1))
-console.log(func(1, false))
-console.log(func(1, true))
 
-async function asyncFunc(): Promise<void> {}
+function foo8(name: string, age: number = 18): number {
+  return name.length + age
+}
+
+function foo9(arg1: string, ...rest: any[]) {}
+function foo10(arg1: string, ...rest: [number, boolean]) {}
+foo10('brad', 100, false)
